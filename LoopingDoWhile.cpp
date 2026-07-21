4c86f2f9d11a3035fdc063e3bc5e3a6bf83fdee9
jeeyyxs <riodwimulyono0512@gmail.com>
2026-07-21 10:55:15 +0700

    perulangan dengan do while

--- LoopingDoWhile.cpp
+++ LoopingDoWhile.cpp
@@ -3,3 +3,22 @@
 #include <ctime>
 using namespace std;
 
+int main() {
+    int x;
+    srand (time(0)); // Seed untuk menghasilkan angka acak
+
+    //Perulangan dengan DO.. WHILE (Minimal 1x dikerjakan)
+    cout << "PERULANGAN DO.. WHILE" << endl;
+    //Nilai awal x = 1
+    x = 1 + rand() % 10; // Menghasilkan angka acak antara 1 hingga 10
+
+    do {
+        cout << "TI, Bilangan acak = " << x << endl;
+        x = 1 + rand() % 10; // Pengubah dari nilai x
+    } while (x <= 5); //Syarat 
+
+    cout << "Bilangan acak do-while yang terakhir = " << x << endl; // Menampilkan nilai terakhir dari x
+    cout << endl;
+
+    return 0;
+}

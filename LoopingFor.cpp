574c4e1e4a84afc138fa8224fb8e3089f352c0a3
jeeyyxs <riodwimulyono0512@gmail.com>
2026-07-21 10:25:45 +0700

    perulangan dengan for untuk naik

--- LoopingFor.cpp
+++ LoopingFor.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+    //Perulangan dengan FOR 
+    cout << "PERULANGAN FOR" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Nilai i : " << i << " Teknologi Informasi UMY " << endl;
+    }
+    cout << endl;
+
+    
+}

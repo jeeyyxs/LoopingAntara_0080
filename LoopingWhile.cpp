0385a396a488f8994543ae37090bbdea4bd0fd3a
jeeyyxs <riodwimulyono0512@gmail.com>
2026-07-21 10:45:23 +0700

    menghasilkan angka acak antara 1 sampai 10

--- LoopingWhile.cpp
+++ LoopingWhile.cpp
@@ -7,5 +7,8 @@ int main() {
     int x;
     srand(time(0)); // Seed untuk menghasilkan angka acak
 
-    
+    // Perulangan dengan WHILE 
+    cout << "PERULANGAN WHILE" << endl;
+    //Nilai awal x = 1
+    x = 1 + rand() % 10; // Menghasilkan angka acak antara 1 hingga 10
 }

96db39b7e213da747a27f43d16a65e6cdf7e595e
jeeyyxs <riodwimulyono0512@gmail.com>
2026-07-21 10:47:29 +0700

    perulangan dengan while

--- LoopingWhile.cpp
+++ LoopingWhile.cpp
@@ -11,4 +11,10 @@ int main() {
     cout << "PERULANGAN WHILE" << endl;
     //Nilai awal x = 1
     x = 1 + rand() % 10; // Menghasilkan angka acak antara 1 hingga 10
-}
+
+    while (x <= 5) {
+        cout << "TI, Bilangan acak = " << x << endl;
+        x = 1 + rand() % 10; // Pengubah dari nilai x
+    }
+
+    
